f069e6c0075266778445327c73d20bf798dd9cfa
Shailesh <vspn8569@gmail.com>
2021-02-03 16:09:32 +0530

    Modified html css and js file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const mainMenu = document.querySelector('.main-menu');
 const navbar = document.querySelector('.navbar');
+const links = document.querySelectorAll('.main-menu ul li a');
 
 let menuOpen = false;
 
@@ -15,6 +16,20 @@ menuBtn.addEventListener('click', () => {
 
     mainMenu.classList.toggle('open');
 })
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log('hello');
+        mainMenu.classList.remove('open');
+        if (!menuOpen) {
+            menuBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            menuBtn.classList.remove('open');
+            menuOpen = false;
+        }
+    })
+});
 
 window.addEventListener('scroll', () => {
     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
